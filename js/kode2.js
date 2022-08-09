02a2f6231e3fd0ad6856bfbd9aec9c6a38d8b121
anandaandikabp <ananda.andika18@gmail.com>
2022-08-09 11:25:39 +0700

    child component

--- js/kode2.js
+++ js/kode2.js
@@ -1,57 +1,66 @@
+const Home = {
+    template: '<div>Home</div>'
+}
+
 Vue.component("header-component", {
-  template: `
-    <header>
-        <img src="./assets/image/1.jpg" alt="img" style="width: 300px;">
-        <p v-text="pesan"></p>
-    </header>
-    `,
-  data: () => {
-    return {
-      pesan: "Hello, Component",
-    };
-  },
+    props: ['nama','image'],
+    template: `
+        <header>
+            <img :src="image" alt="img" style="width: 300px;">
+            <p>{{ 'Hello ' +nama }}</p>
+        </header>
+        `,
+    data: () => {
+        return {
+        pesan: "Hello, Component",
+        };
+    },
 });
 
 Vue.component("footer-component", {
-  template: `
-    <footer id="footer">
-        <p>copyright 2022</p>
-    </footer>
-    `,
+    template: `
+        <footer id="footer">
+            <p>copyright 2022</p>
+        </footer>
+        `,
 });
 
 Vue.component("kelas", {
-  template: `
-  <div>
-    <h3>Form Submit.</h3>
-    <p><input type="text" placeholder="Nama Kelas" v-on:keyup.enter="submit" v-model="kelasBaru"></p>
+    props: ['items','input'],
+    template: `
+    <div>
+        <h3>Form Submit.</h3>
+        <p><input type="text" placeholder="Nama Kelas" v-on:keyup.enter="submit" v-model="input"></p>
 
-    <h3>Daftar Kelas {{ kelas.length }}</h3>
-    <template v-if="kelas.length >= 1">
-        <ul>
-            <li v-for="(item, index) of kelas">{{ index+1 }} - {{ item }}</li>
-        </ul>
-    </template>
-    <li v-else>Kelas Belum Tersedia</li>
-  </div>    
-    `,
-  data: () => {
-    return {
-      kelas: ["Xiaomi", "Oppo", "Vivo"],
-      kelasBaru: "",
-    };
-  },
-  methods: {
-    submit: function (event) {
-      let text = event.target.value;
-      console.log(text);
-      this.kelas.unshift(this.kelasBaru);
-      this.kelasBaru = "";
+        <h3>Daftar Kelas {{ items.length }}</h3>
+        <template v-if="items.length >= 1">
+            <ul>
+                <li v-for="(item, index) of items">{{ index+1 }} - {{ item }}</li>
+            </ul>
+        </template>
+        <li v-else>Kelas Belum Tersedia</li>
+    </div>    
+        `,
+    methods: {
+        submit: function (event) {
+        let text = event.target.value;
+        console.log(text);
+        this.kelas.unshift(this.kelasBaru);
+        this.kelasBaru = "";
+        },
     },
-  },
 });
 
 const vm = new Vue({
-  el: "#app",
-  computed: {},
+    el: "#app",
+    components: {
+        'home': Home
+    },
+    data: function() {
+        return {
+        kelas: ["Xiaomi", "Oppo", "Vivo"],
+        kelasBaru: "",
+        };
+    },
+    computed: {},
 });
