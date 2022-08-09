6f38df3dd5693e2c852af056abec15ec828e7aed
anandaandikabp <ananda.andika18@gmail.com>
2022-08-09 10:35:03 +0700

    membuat component kelas

--- js/kode2.js
+++ js/kode2.js
@@ -2,8 +2,14 @@ Vue.component("header-component", {
   template: `
     <header>
         <img src="./assets/image/1.jpg" alt="img" style="width: 300px;">
+        <p v-text="pesan"></p>
     </header>
     `,
+  data: () => {
+    return {
+      pesan: "Hello, Component",
+    };
+  },
 });
 
 Vue.component("footer-component", {
@@ -14,11 +20,26 @@ Vue.component("footer-component", {
     `,
 });
 
-const vm = new Vue({
-  el: "#app",
-  data: {
-    kelas: ["Xiaomi", "Oppo", "Vivo"],
-    kelasBaru: "",
+Vue.component("kelas", {
+  template: `
+  <div>
+    <h3>Form Submit.</h3>
+    <p><input type="text" placeholder="Nama Kelas" v-on:keyup.enter="submit" v-model="kelasBaru"></p>
+
+    <h3>Daftar Kelas {{ kelas.length }}</h3>
+    <template v-if="kelas.length >= 1">
+        <ul>
+            <li v-for="(item, index) of kelas">{{ index+1 }} - {{ item }}</li>
+        </ul>
+    </template>
+    <li v-else>Kelas Belum Tersedia</li>
+  </div>    
+    `,
+  data: () => {
+    return {
+      kelas: ["Xiaomi", "Oppo", "Vivo"],
+      kelasBaru: "",
+    };
   },
   methods: {
     submit: function (event) {
@@ -28,5 +49,9 @@ const vm = new Vue({
       this.kelasBaru = "";
     },
   },
+});
+
+const vm = new Vue({
+  el: "#app",
   computed: {},
 });
